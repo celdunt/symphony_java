efa5b36b6400e01b3c2d5cf68d8728490d0afffa
xel dunt <s1zceero@gmail.com>
2023-09-14 10:16:09 +0300

    09.14.2023 -- master

--- src/main/java/loc/ex/symphony/search/Searcher.java
+++ src/main/java/loc/ex/symphony/search/Searcher.java
@@ -8,6 +8,7 @@ import loc.ex.symphony.listview.Book;
 import loc.ex.symphony.listview.Link;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.ConcurrentHashMap;
@@ -38,164 +39,27 @@ public class Searcher {
 
         if (indexData != null) {
             String[] words = prompt.split("[\\s\\p{Punct}]+");
-            List<List<IndexStruct>> wordsData = new ArrayList<>();
+            List<List<IndexStruct>> primaryWordsList = new ArrayList<>();
+            List<List<IndexStruct>> synonymsWordsList = new ArrayList<>();
+
 
             for (String word : words) {
-                List<IndexStruct> another = indexData.get(word.toLowerCase());
-                for (String synonym : another.get(0).getSynonyms()) another.addAll(indexData.get(synonym));
-                wordsData.add(another);
+                List<IndexStruct> primaryWords = indexData.get(word.toLowerCase());
+                List<IndexStruct> synonymsWords = new ArrayList<>();
+                for (String synonym : primaryWords.get(0).getSynonyms()) synonymsWords.addAll(indexData.get(synonym));
+
+                Collections.sort(primaryWords);
+                Collections.sort(synonymsWords);
+
+                primaryWordsList.add(primaryWords);
+                synonymsWordsList.add(synonymsWords);
             }
 
-
-            List<IndexStruct> references = new ArrayList<>();
-            List<ListIterator<IndexStruct>> iterators = new ArrayList<>();
-
-            for (List<IndexStruct> wordData : wordsData) {
-                iterators.add(wordData.listIterator());
-                references.add(wordData.get(0));
-            }
-
-            while (adjustBookID(references, iterators)) {
-                foundOccurrences.add(new Link(references, resource, words));
-            }
+            // реализовать функцию сочетания данных -> берем 0-вой элемент и подбираем под него подходящие прочие элементы.
 
         }
 
 
         return FXCollections.observableArrayList(foundOccurrences);
-    }
-
-
-    /**
-     *
-     * Returns "false" if them couldn't find equal parameters "bookID"
-     *
-     * @param references This parameter will be changed so that all elements of this parameter will have the same "bookID" parameter
-     * @param iterators This parameter stores a list of IndexStruct
-     *
-     */
-    private boolean adjustBookID(List<IndexStruct> references, List<ListIterator<IndexStruct>> iterators) {
-        for (int iref = 0; iref < references.size(); iref++) {
-            int countEquals = 0;
-
-            for (int jref = 0; jref < references.size(); jref++) {
-                if (iref != jref) {
-
-                    if (references.get(iref).getBookID() < references.get(jref).getBookID()) {
-                        while(references.get(iref).getBookID() < references.get(jref).getBookID()) {
-                            if (iterators.get(iref).hasNext())
-                                references.set(iref, iterators.get(iref).next());
-                            else
-                                return false;
-                        }
-                        break;
-                    }
-                    while (references.get(jref).getBookID() < references.get(iref).getBookID()) {
-                        if (iterators.get(jref).hasNext())
-                            references.set(jref, iterators.get(jref).next());
-                        else
-                            return false;
-                    }
-                    if (references.get(iref).getBookID() == references.get(jref).getBookID()) countEquals++;
-                }
-            }
-
-            if (countEquals == references.size()-1) {
-                return adjustChapterID(references, iterators);
-            }
-
-        }
-
-        return false;
-    }
-
-
-    /**
-     *
-     * Returns "false" if them couldn't find equal parameters "chapterID"
-     *
-     * @param references This parameter will be changed so that all elements of this parameter will have the same "chapterID" parameter
-     * @param iterators his parameter stores a list of IndexStruct
-     *
-     */
-    private boolean adjustChapterID(List<IndexStruct> references, List<ListIterator<IndexStruct>> iterators) {
-        for (int iref = 0; iref < references.size(); iref++) {
-            int countEquals = 0;
-
-            for (int jref = 0; jref < references.size(); jref++) {
-                if (iref != jref) {
-
-                    if (references.get(iref).getChapterID() < references.get(jref).getChapterID()) {
-                        while(references.get(iref).getChapterID() < references.get(jref).getChapterID()) {
-                            if (iterators.get(iref).hasNext())
-                                references.set(iref, iterators.get(iref).next());
-                            else
-                                return false;
-                        }
-                        break;
-                    }
-                    while (references.get(jref).getChapterID() < references.get(iref).getChapterID()) {
-                        if (iterators.get(jref).hasNext())
-                            references.set(jref, iterators.get(jref).next());
-                        else
-                            return false;
-                    }
-                    if (references.get(iref).getChapterID() == references.get(jref).getChapterID()) countEquals++;
-
-                }
-            }
-
-            if (countEquals == references.size()-1) {
-                return adjustFragmentID(references, iterators);
-            }
-
-        }
-
-        return false;
-    }
-
-    /**
-     *
-     * Returns "false" if them couldn't find equal parameters "fragmentID"
-     *
-     * @param references This parameter will be changed so that all elements of this parameter will have the same "fragmentID" parameter
-     * @param iterators his parameter stores a list of IndexStruct
-     *
-     */
-    private boolean adjustFragmentID(List<IndexStruct> references, List<ListIterator<IndexStruct>> iterators) {
-        for (int iref = 0; iref < references.size(); iref++) {
-            int countEquals = 0;
-
-            for (int jref = 0; jref < references.size(); jref++) {
-                if (iref != jref) {
-
-                    if (references.get(iref).getFragmentID() < references.get(jref).getFragmentID()) {
-                        while(references.get(iref).getFragmentID() < references.get(jref).getFragmentID()) {
-                            if (iterators.get(iref).hasNext())
-                                references.set(iref, iterators.get(iref).next());
-                            else
-                                return false;
-                        }
-                        break;
-                    }
-                    while (references.get(jref).getFragmentID() < references.get(iref).getFragmentID()) {
-                        if (iterators.get(jref).hasNext())
-                            references.set(jref, iterators.get(jref).next());
-                        else
-                            return false;
-                    }
-                    if (references.get(iref).getFragmentID() == references.get(jref).getFragmentID()) countEquals++;
-
-                }
-            }
-
-            if (countEquals == references.size()-1) {
-                references.set(iref, iterators.get(iref).next());
-                return true;
-            }
-
-        }
-
-        return false;
     }
 }
